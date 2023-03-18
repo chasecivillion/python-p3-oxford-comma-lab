33b69cfe4cb2d2022d1365e3261cf6081a8ff993
Beau Kim <beaukim3@gmail.com>
2023-03-18 00:30:14 -0400

    finished

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,14 @@
+# import ipdb
+
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return "".join(items)
+    if len(items) == 2:
+        return " and ".join(items)
+    elif len(items) > 2:
+        except_last =  ", ".join(items[0:len(items) - 1])
+        return except_last + f', and {items[-1]}'
+        
+# items = ['kiwi', 'durian', 'starfruit', 'mangos', 'dragon fruits']
+
+# ipdb.set_trace()
